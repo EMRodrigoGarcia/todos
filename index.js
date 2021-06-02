a703695a58495fa43b57ee5e0dfd2859ffc6b6e3
Rodrigo Garcia <rglopez300@gmail.com>
2021-06-02 13:08:16 +0200

    string a array

--- index.js
+++ index.js
@@ -3,6 +3,26 @@ const http = require('http');
 
 var gd = require('querystring');
 
+
+function arrayToString(tokenStr) {
+    var tokenList = [];
+    var aux = "";
+    // pasar de string a array de strings
+    for (const iterator of tokenStr) {
+        if (iterator != "[" && iterator != "," && iterator != " " && iterator != "]") {
+            aux += iterator;
+            console.log(aux);
+        } else {
+            // comprobar que tenemos valor
+            if (aux) {
+                tokenList.push(aux);
+                aux = "";
+            }
+        }
+    }
+
+    return tokenList;
+}
 admin.initializeApp({
     credential: admin.credential.applicationDefault(),
     databaseURL: "https://todos-c9840-default-rtdb.europe-west1.firebasedatabase.app/"
@@ -12,42 +32,43 @@ var db = admin.database();
 
 //https://stackoverflow.com/questions/4295782/how-to-process-post-data-in-node-js
 const server = http.createServer(function (request, response) {
-    //TODO: Adaptar el código de atrapado para poder pasar arrays
     if (request.method == "POST") {
         console.log("Algo por POST");
         var body = [];
 
         request.on('data', function (data) {
-            console.log(data);
             body.push(data);
-
         });
 
         request.on('close', () => {
+            //TODO: Encontrar mejor manera de hacerlo...
             console.log("Se acabo");
-            var token = Buffer.concat(body).toString();
-            console.log(token);
+            var tokenStr = Buffer.concat(body).toString();
 
-            var message = {
-                notification: {
-                    title: 'Un contacto ha tenido COVID-19',
-                    body:'Un contacto ha tenido COVID-19'
-                },
-                token: token
-            };
+            arrayToString(tokenStr).forEach(element => {
+                var message = {
+                    notification: {
+                        title: 'Un contacto ha tenido COVID-19',
+                        body: 'Un contacto ha tenido COVID-19'
+                    },
+                    token: element
+                };
 
-            //TODO: Comprobar si el dato que nos estan pasando esta en BBDD
-            admin.messaging().send(message).then((res) => {
-                console.log("De puta madre socio " + res);
-            }).catch((err) => {
-                console.error("puta mierda " + err);
+                //TODO: Comprobar si el dato que nos estan pasando esta en BBDD
+                admin.messaging().send(message).then((res) => {
+                    console.log("De puta madre socio " + res);
+                }).catch((err) => {
+                    console.error("puta mierda " + err);
+                });
+
             });
-        });
-
+        }
+        );
     } else {
         console.log("Otra cosa");
     }
-})
+
+});
 
 
 const port = 3000;
