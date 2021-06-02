e7b621b24830e8cdb4c243239a0cd5c0a6e71318
Rodrigo Garcia <rglopez300@gmail.com>
2021-06-02 11:42:31 +0200

    notificaciones desde server HECHO

--- index.js
+++ index.js
@@ -1,26 +1,50 @@
-//var admin = require('firebase-admin');
+var admin = require('firebase-admin');
 const http = require('http');
 
 var gd = require('querystring');
 
+admin.initializeApp({
+    credential: admin.credential.applicationDefault(),
+    databaseURL: "https://todos-c9840-default-rtdb.europe-west1.firebasedatabase.app/"
+});
+
+var db = admin.database();
+
 //https://stackoverflow.com/questions/4295782/how-to-process-post-data-in-node-js
 const server = http.createServer(function (request, response) {
+    //TODO: Adaptar el código de atrapado para poder pasar arrays
     if (request.method == "POST") {
         console.log("Algo por POST");
         var body = [];
 
         request.on('data', function (data) {
             console.log(data);
-            body.push(data); 
+            body.push(data);
 
         });
 
         request.on('close', () => {
             console.log("Se acabo");
-            console.log(Buffer.concat(body).toString());
+            var token = Buffer.concat(body).toString();
+            console.log(token);
+
+            var message = {
+                notification: {
+                    title: 'Un contacto ha tenido COVID-19',
+                    body:'Un contacto ha tenido COVID-19'
+                },
+                token: token
+            };
+
+            //TODO: Comprobar si el dato que nos estan pasando esta en BBDD
+            admin.messaging().send(message).then((res) => {
+                console.log("De puta madre socio " + res);
+            }).catch((err) => {
+                console.error("puta mierda " + err);
+            });
         });
 
-    }else {
+    } else {
         console.log("Otra cosa");
     }
 })
@@ -32,18 +56,12 @@ const host = "192.168.1.43";
 try {
     server.listen(port, host)
 } catch (error) {
-   console.log(error); 
+    console.log(error);
 }
 console.log("Escuchando en " + host + ":" + port);
 
 
-// admin.initializeApp({
-//     credential:admin.credential.applicationDefault(),
-//     databaseURL:'https://todos-c9840-default-rtdb.europe-west1.firebasedatabase.app/'
-// });
 
-// var db = admin.database();
-// var ref = db.ref("/hola");
 
 // ref.once('value', data => {
 //     console.log(data.val());
@@ -51,13 +69,7 @@ console.log("Escuchando en " + host + ":" + port);
 
 // var topic = 'all';
 
-// var message = {
-//     notification:{
-//         title:'Hola a todos',
-//         body:'Hola doctor nick'
-//     },
-//     topic:topic
-// };
+
 
 // admin.messaging().send(message).then((response) => {
 //     console.log("Enviado de puta madre socio " + response);
